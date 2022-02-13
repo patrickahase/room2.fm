4b7d9c068e3052de9fee9dd59339cec64f69ecbf
hase <patrickahase@gmail.com>
2022-02-13 15:55:24 +1100

    new build

--- client/src/content/modalText.js
+++ client/src/content/modalText.js
@@ -1,18 +1,16 @@
 export const acknowledgementOfCountryText = <span>
-  This website was created and is maintained on the unceded territory of the Wurundjeri people of the Kulin Nations, 
+  This website was created and is maintained on the unceded territory of the Wurundjeri People of the Kulin Nations, 
   the Traditional Custodians of the land. We recognise their ongoing legacy of connection to land, waters and culture 
-  and pay respect to their Elders past, present and emerging. We extend this respect to those of the wider Kulin Nations 
-  along with all other First Nations peoples of this continent, whose stolen land the material pathways that allow our 
-  digital connectivity are built upon.
+  and pay respect to their Elders past, present and emerging. We extend this respect to all other First Nations peoples 
+  of this continent, whose stolen land the material pathways that allow our digital connectivity are built upon.
 </span>
 
 export const introText = <>
   <span>
-    room2.fm seeks to create a digital space for collective reflection, sharing, and vulnerability. Izabel, Hani Hanbali and 
-    Hannah Wu will be selecting sounds tonight and are joined by Emile Frankel and Panda Wong taking over the prompts and Mohamed 
-    Chamas taking over the visuals.
-    <br />
-    8pm =&gt; 10pm (AEST)
+    room2.fm seeks to create a digital space for collective reflection, sharing, and vulnerability. During the last lockdown 
+    season we were lucky to have a range of guest artists who helped facilitate this by contributing sounds, texts and visuals 
+    to the space, which was brought together by the active participation in the responses and in the chat by those attending 
+    the space on each given weekly event.
   </span>
   <br />  
   <br />
@@ -26,8 +24,8 @@ export const introText = <>
     This website has been optimised to run with the window maximised on Chrome and Firefox on a desktop or laptop screen. 
     If you are having issues, please try one of these browsers.
     <br />
-    If you would like a high-contrast version of the website's text with no distortion please click 'Focus Mode' button.
-    <br />    
+   {/*  If you would like a high-contrast version of the website's text with no distortion please click 'Focus Mode' button.
+    <br />     */}
     We will be moderating responses that we feel are intentionally disruptive or may make others feel unsafe in the space.
   </span>
   <br />
@@ -48,11 +46,17 @@ export const instructionsText = <>
   <br />
   <br />
   <span>
-    Periodically, prompts will come up in the lower middle of the screen. Some of these will request a written response, 
-    some a drawn response. You can write or draw your response in the white box at the bottom of the screen before clicking 
-    the ‘submit response’ button to share it with others. Your responses to these prompts will be completely anonymised and 
-    not attributable to you in any way. Above the ‘submit response’ button, you can select the colour palette you will be 
-    able to draw responses with.
+    Periodically, text prompts will come up in the lower middle of the screen. Underneath the prompts you can select to respond 
+    via either writing or drawing depending on what you think is most appropriate. Once you are happy with your response you can 
+    click the ‘submit response’ button to share it with others. Above the ‘submit response’ button, you can select the colour palette 
+    you will be able to draw responses with, along with options to change the brush size, use an eraser and undo or redo additions to 
+    your drawing. 
+  </span>
+  <br />
+  <br />
+  <span>
+    Your responses to these prompts will be completely anonymised and not attributable to you in any way, however during this 
+    demonstration we will be capturing a screen recording of the experience.
   </span>
   <br />
   <br />
