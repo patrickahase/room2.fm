2fba20470f1912ddd7c62f5bbc80c7573a964b5d
hase <patrickahase@gmail.com>
2022-07-07 12:04:35 +1000

    start continue button

---
+++ client/src/components/audioTrackPlayer.js
@@ -0,0 +1,246 @@
+import React, { useEffect, useState } from 'react';
+
+/* set amount of volume notches on volume bar */
+var notchNumber = 12;
+
+export default function AudioTrackPlayer(props) {
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const [audioSource, setAudioSource] = useState(null);
+
+  useEffect(() => {
+    //set reference to audio
+    let audioSourceRef = document.getElementById("audio-source");
+    setAudioSource(audioSourceRef);
+    props.setAudioSourceRef(audioSourceRef);
+    //set reference to timeline
+    let timeline = document.getElementById("timeline");
+    //add waiting animation
+    audioSourceRef.addEventListener('waiting', () => {
+      timeline.className = "";
+      timeline.className = "timelineloading";
+    });
+    audioSourceRef.addEventListener('playing', () => {
+      timeline.className = "";
+      timeline.className = "timelineloaded";
+    });
+    //add animation when playing
+    audioSourceRef.addEventListener('timeupdate', () => {
+      timeline.firstChild.style.width = (100 * audioSourceRef.currentTime/audioSourceRef.duration) + "%";
+    });
+    //add timeline click
+    timeline.addEventListener("click", (e) => {
+      //get percentage of where timeline clicked
+      let clickPercent = (e.clientX - timeline.getBoundingClientRect().left) / timeline.offsetWidth;
+      // set playhead
+      timeline.firstChild.style.width = (100 * clickPercent) + "%";
+      // set new track time
+      audioSourceRef.currentTime = audioSourceRef.duration * clickPercent;
+    })
+  }, []);
+
+  return (
+    <div id="audio-player-wrapper">
+      {/* audio source */}
+      <audio src={require("../content/sade.mp3").default} id="audio-source" />
+      {/* play button */}
+      <button id="play-pause-button" className="AudioPlayerButton" onClick={playPauseAudio}>
+        {isPlaying
+            ? <PauseIcon />
+            : <PlayIcon /> }
+      </button>
+      {/* reset button */}
+      <button className="AudioPlayerButton" onClick={() => {audioSource.currentTime = 0; props.restartTimer()}}>
+        <RewindIcon />
+      </button>
+      {/*Audio Timeline*/}
+      <div id="timeline" className="timelineloaded">
+        <div id="playhead" />
+      </div>
+      {/* volume section */}
+      <div id="volume-section-wrapper">
+        {/* mute button */}
+        <button className="AudioPlayerButton" id="mute-button" onClick={muteAudio}>
+          {isMuted
+            ? <MuteIconOff />
+            : <MuteIconOn /> }                    
+        </button>
+        {/* volume slider */}
+        <div id="volume-wrapper">
+            {VolumeNotches}
+        </div>
+      </div>               
+    </div>
+  )
+  function playPauseAudio(){
+    if(audioSource.paused){
+      audioSource.play();
+      setIsPlaying(true);
+      props.startTimer();
+      props.setAskResponse(true);
+    } else {
+      audioSource.pause();
+      setIsPlaying(false);
+      props.pauseTimer();
+    }
+  }  
+  function muteAudio(){
+    audioSource.muted = !audioSource.muted;
+    setIsMuted(!isMuted);
+  }
+}
+
+let volNotchGen = Array(notchNumber).fill('');
+
+/* can this be changed to button? */
+
+const VolumeNotches = volNotchGen.map((nothing, index) =>
+  <div className='VolumeNotch VolumeActive' id={index} 
+       onMouseDown={clickVolumeNotch}
+       onMouseEnter={(e) => {if(isMouseDown){clickVolumeNotch(e)}}}
+       style={{height: 100/volNotchGen.length + "%"}} />
+)
+
+/* keep track of mouse down for volume drag event */
+var isMouseDown = false;
+document.addEventListener("mouseup", () => {isMouseDown = false});
+
+function clickVolumeNotch(e){
+  e.stopPropagation();
+  isMouseDown = true;
+  let clickedID = parseInt(e.target.id);
+  /* set classes for active notch */
+  let notches = document.getElementsByClassName("VolumeNotch");
+  for (let notch of notches){
+    if(notch.id < clickedID){
+      notch.classList.remove("VolumeActive");
+    } else {
+      notch.classList.add("VolumeActive");
+    }  
+  }
+  /* set new volume */
+  let newVolume = 1 - (1/notchNumber) * clickedID;
+  document.getElementById("audio-source").volume = newVolume;
+}
+
+function PlayIcon(){
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 110 110"
+      xmlns="http://www.w3.org/2000/svg"
+      transform="">
+        <g transform="scale(.8) translate(15)" style={{transformOrigin: "center"}} className="AudioPlayerIcon">
+          <polygon points="10,10 73,55 10,100"
+                   strokeLinejoin="round"
+                   strokeWidth="20"
+          />
+        </g>        
+    </svg>
+  )
+}
+function PauseIcon(){
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 110 110"
+      xmlns="http://www.w3.org/2000/svg"
+      transform="">
+        <g transform="scale(.8) translate(15)" style={{transformOrigin: "center"}} className="AudioPlayerIcon">
+          <path strokeLinecap="round" strokeWidth="30"
+              d=" M 67.5,15
+                  L 67.5,95" />
+          <path strokeLinecap="round" strokeWidth="30"
+              d=" M 15,15
+                  L 15,95" />
+        </g>        
+    </svg>
+  )
+}
+function RewindIcon(){
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 110 110"
+      xmlns="http://www.w3.org/2000/svg"
+      transform="">
+        <g transform="scale(.8)" style={{transformOrigin: "center"}} className="AudioPlayerIcon">
+          <polygon points="10,10 73,55 10,100"
+                   strokeLinejoin="round"
+                   strokeWidth="20"
+                   transform="translate(-25) rotate(180, 55, 55)"
+          />
+          <path fill="none" strokeLinecap="round" strokeWidth="20"
+              d=" M 100,10
+                  L 100,100" />
+        </g>
+               
+    </svg>
+  )
+}
+function MuteIconOn(){
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+      transform=""
+      className="AudioPlayerIcon">
+        {/* Speaker */}
+        <path id="mute-speaker"
+              d=" M 10,42
+              A 2,2 90 0 1 12,40
+              L 15,40 30,15 35,15
+              Q 38,50 35,85 
+              L 30,85 15,60 12,60
+              A 2,2 90 0 1 10,58
+              Z              
+        " />
+        {/* Volume Lines */}
+        <path fill="none" strokeLinecap="round" strokeWidth="6" className="MuteLines active"
+              d=" M 45,35
+                  Q 55,50 45,65" />
+        <path fill="none" strokeLinecap="round" strokeWidth="6" className="MuteLines active"
+              d=" M 55,22.5
+                  Q 77.33,50 55,77.5" />
+        <path fill="none" strokeLinecap="round" strokeWidth="6" className="MuteLines active"
+              d=" M 65,10
+                  Q 101.66,50 65,90" />
+    </svg>
+  )
+}
+function MuteIconOff(){
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+      opacity="50%"
+      className="AudioPlayerIcon">
+        {/* Speaker */}
+        <path id="mute-speaker"
+              d=" M 10,42
+              A 2,2 90 0 1 12,40
+              L 15,40 30,15 35,15
+              Q 38,50 35,85 
+              L 30,85 15,60 12,60
+              A 2,2 90 0 1 10,58
+              Z              
+        " />
+        <path fill="none" strokeWidth="6" 
+              d=" M 50, 35
+                  L 80, 65
+              " />
+        <path fill="none" strokeWidth="6"
+              d=" M 80, 35
+                  L 50, 65
+              " />
+    </svg>
+  )
+}
--- client/src/components/desktopApp.js
+++ client/src/components/desktopApp.js
@@ -1,169 +1,74 @@
-import React, { Component } from 'react';
-import AudioControls from './audioControls';
-import DrawingCanvas from './drawingCanvas';
+import React, { useEffect, useState } from 'react';
 import IntroModal from './introModal';
-import DrawingTools from './drawingTools';
-import SettingsMenu from './settingsMenu';
 import GLVis from './glVis';
 import ResponseDisplay from './responseDisplay';
-import { InfoOverlay } from './infoOverlay';
-import ColourPicker from './colourPicker';
 import AudioTrackPlayer from './audioTrackPlayer';
 
-export class DesktopApp extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      
-      isMuted: false,
+/* set timer refresh rate in ms */
+const timerRefreshRate = 50;
 
-      //video stream player
-      streamPlayer: null,
+export default function DesktopApp(props) {
 
-      // emoji triangle values
-      emojiX: 0.5,
-      emojiY: 0.5,
+  const[askResponse, setAskResponse] = useState(false);
+  const[readyToRespond, setReadyToRespond] = useState(false);
 
-      timer: 0.,
+  const[timer, setTimer] = useState(0.);
+  const[timerInterval, setTimerInterval] = useState(0.);
+  const[audioSourceRef, setAudioSourceRef] = useState(null);
 
-      infoOverlay: true
-
-    }
-    this.setStreamPlayer = this.setStreamPlayer.bind(this);
-    this.setGainControl = this.setGainControl.bind(this);
+  return (
+    <div id="desktop-wrapper">
+      {props.modalIsOpen
+        /* if true show modal */
+        ? <IntroModal
+            mobile={props.mobile}
+            toggleModal={props.toggleModal} />
+        /* else show the desktop app */
+        : <>
+          {/* Background Visuals */}
+          <div id="bg-vis-wrapper">              
+            <GLVis
+              timer={timer}
+              height={props.height}
+              width={props.width} />          
+          </div>
+          {/* External Response Overlay */}
+          <ResponseDisplay 
+            getNextResponse={props.getNextResponse}
+            responsesToDisplay={props.responsesToDisplay}
+            height={props.height}
+            width={props.width} />
+          {/* Audio Player */}
+          <AudioTrackPlayer
+            setAudioSourceRef={setAudioSourceRef}
+            startTimer={startTimer}
+            pauseTimer={pauseTimer}
+            restartTimer={restartTimer}
+            setAskResponse={setAskResponse}
+            />
+          {/* Ask if ready to respond */} 
+          {askResponse &&
+            <button id="ready-submission-button"> submit response </button>
+          }
+          {/* Show response input */}
+          {readyToRespond &&
+            <div id="input-wrapper"></div>
+          }
+          </>
+      }
+    </div>
+  )
+  /* update timer value (based on track position) */
+  function startTimer() {
+    setTimerInterval(setInterval(() => {
+      setTimer(100 * audioSourceRef.currentTime/audioSourceRef.duration)
+    }, timerRefreshRate));
   }
-  render() {
-    return (
-      <div id="desktop-wrapper">
-        {this.props.modalIsOpen
-          ? <IntroModal 
-              mobile={this.props.mobile}
-              toggleModal={this.props.toggleModal} />
-          : <>      
-            <div id="bg-vis-wrapper">              
-              <GLVis
-                timer={this.state.timer}
-                height={this.props.height}
-                width={this.props.width} />          
-            </div>
-            {/* Response Overlay */}
-            <ResponseDisplay 
-              getNextResponse={this.props.getNextResponse}
-              responsesToDisplay={this.props.responsesToDisplay}
-              height={this.props.height}
-              width={this.props.width} />
-            <AudioTrackPlayer />
-            {/* Menu Overlay */}
-            {/* <SettingsMenu
-              toggleFocus={this.props.toggleFocus} 
-              overlayToggle={this.toggleInfoOverlay.bind(this)} /> */}
-            {/* Info Overlay */}
-            {/* {this.state.infoOverlay &&
-              <InfoOverlay overlayToggle={this.toggleInfoOverlay.bind(this)} /> } */}
-            {/* Prompt and Input Selection Overlay */}
-            {/* <div id="current-prompt-wrapper">
-              <div id="prompt-end-timer-wrapper">
-                <div id="prompt-end-timer" />
-                <div id="prompt-end-timer-overlay" />
-              </div>           
-              <div id="current-prompt">
-                {this.props.currentPrompt}
-              </div>           
-              <div id="input-select-wrapper">
-                <span>I would like to&nbsp;
-                <button id="draw-input-select" className="InputSelectButton" onClick={this.props.setDrawInput}>draw</button>&nbsp;/&nbsp;
-                <button id="text-input-select" className="InputSelectButton ActiveInputButton" onClick={this.props.setWriteInput}>write</button>
-                &nbsp;a response</span>
-              </div>           
-            </div>  */}                    
-            {/* Input Selection */}
-            
-            {/* Input Section */}
-            {/* <div id="input-wrapper"> */}              
-              <>
-                {/* <textarea id="text-input" name="text based prompt response" placeholder="Please type your response here..." /> */}
-              </>
-              <>{/* Drawing Input */}
-                {/* <DrawingCanvas 
-                  brushColour={this.props.colours.colour1}
-                  brushSize={this.props.brushSize}
-                  setCanvas={this.props.setCanvas}
-                  setIsDrawing={this.props.setIsDrawing}
-                  /> */}
-              </>
-                
-            {/* </div> */}
-            {/* <DrawingTools
-                undoDrawing={this.props.undoDrawing}
-                redoDrawing={this.props.redoDrawing}
-                toggleEraser={this.props.toggleEraser}
-                colours={this.props.colours}
-                changeColourOrder={this.props.changeColourOrder}
-                changeColours={this.props.changeColours}
-                changeBrushSize={this.props.changeBrushSize} />  */} 
-            {/* Right UI Panel */}
-            <div id="right-ui-wrapper">
-            <div id="drawing-tools-wrapper">                
-                {/* <ColourPicker 
-                  colours={this.props.colours}
-                  changeColourOrder={this.props.changeColourOrder}
-                  changeColours={this.props.changeColours}
-                /> */}
-                
-            </div>
-                          
-              {/* <button id="response-submit-button" onClick={this.props.submitResponse}>
-                <span>SUBMIT RESPONSE</span>                
-              </button> */}
-              {/* Audio Settings */}
-              {/* <AudioControls
-                muteAudio={this.muteAudio.bind(this)}
-                isMuted={this.state.isMuted}
-                changeVolume={this.changeVolume.bind(this)}
-                />
-              <AudioStreamPlayer
-                setStreamPlayer={this.setStreamPlayer}
-                setGainControl={this.setGainControl} />   */}           
-            </div>
-          </>}
-      </div>
-    )
+  function pauseTimer() {
+    clearInterval(timerInterval);    
   }
-  componentDidMount() {  
-    // update css style sheet
-    this.startTimer();
-  }
-  startTimer() {
-    this.myInterval = setInterval(() => {
-      this.setState(prevState =>({
-        timer: prevState.timer + 0.01
-      }))
-    }, 50)     
-  }
-  setStreamPlayer(streamPlayer){
-    this.setState({ streamPlayer: streamPlayer })
-  }
-  setGainControl(gainControl){
-    this.setState({ gainControl: gainControl })
-  }
-  muteAudio(){
-    let streamPlayer = this.state.streamPlayer;
-    if(this.state.isMuted){
-      this.setState({ isMuted: false });
-      streamPlayer.muted = false;
-    } else {
-      this.setState({ isMuted: true });
-      streamPlayer.muted = true;
-    }    
-  }
-  changeVolume(newVolume){
-    /* this.state.streamPlayer.volume(newVolume); */
-    this.state.gainControl.gain.setValueAtTime(newVolume, this.state.streamPlayer.currentTime);
-  }
-  toggleInfoOverlay(){
-    this.setState(prevState => ({infoOverlay: !prevState.infoOverlay}))
+  function restartTimer() {
+    setTimer(0.);    
   }
   
 }
-
-export default DesktopApp
