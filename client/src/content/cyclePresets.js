ce42ea72e56452351a16a3c82ec6b807022dbb60
hase <patrickahase@gmail.com>
2022-08-06 13:18:23 +1000

    added date checker

--- client/src/content/cyclePresets.js
+++ client/src/content/cyclePresets.js
@@ -15,3 +15,82 @@ export const cyclePresets = [
     ]
   }
 ]
+
+// these dates are for testing
+export const cycleDates = [
+  {
+    endTime: Date.parse("2022-08-06T17:52+10:00"),
+    tidalData: [
+      {
+        tideEnd: Date.parse("2022-08-06T13:15+10:00"),
+        tideUp: true,
+        tideHeight: 0.8
+      },
+      {
+        tideEnd: Date.parse("2022-08-06T13:17+10:00"),
+        tideUp: false,
+        tideHeight: 0.2
+      },
+      {
+        tideEnd: Date.parse("2022-08-06T13:19+10:00"),
+        tideUp: true,
+        tideHeight: 0.9
+      },
+      {
+        tideEnd: Date.parse("2022-08-06T13:21+10:00"),
+        tideUp: false,
+        tideHeight: 0.3
+      },
+    ]
+  },
+  {
+    endTime: Date.parse("2022-08-07T17:52+10:00"),
+    tidalData: [
+      {
+        tideEnd: Date.parse("2022-08-06T12:52+10:00"),
+        tideUp: true,
+        tideHeight: 0.8
+      },
+      {
+        tideEnd: Date.parse("2022-08-06T12:52+10:00"),
+        tideUp: false,
+        tideHeight: 0.2
+      },
+      {
+        tideEnd: Date.parse("2022-08-06T12:52+10:00"),
+        tideUp: true,
+        tideHeight: 0.9
+      },
+      {
+        tideEnd: Date.parse("2022-08-06T12:52+10:00"),
+        tideUp: false,
+        tideHeight: 0.3
+      },
+    ]
+  },
+  {
+    endTime: Date.parse("2022-08-08T17:52+10:00"),
+    tidalData: [
+      {
+        tideEnd: Date.parse("2022-08-06T12:52+10:00"),
+        tideUp: true,
+        tideHeight: 0.8
+      },
+      {
+        tideEnd: Date.parse("2022-08-06T12:52+10:00"),
+        tideUp: false,
+        tideHeight: 0.2
+      },
+      {
+        tideEnd: Date.parse("2022-08-06T12:52+10:00"),
+        tideUp: true,
+        tideHeight: 0.9
+      },
+      {
+        tideEnd: Date.parse("2022-08-06T12:52+10:00"),
+        tideUp: false,
+        tideHeight: 0.3
+      },
+    ]
+  },
+]
