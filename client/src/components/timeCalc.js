51b35a8bb8aef4764dcfc1d73e57a61f660e352d
Bec Fary <rebecca.fary@gmail.com>
2022-08-18 19:26:57 +1000

    'where are we'/Native Land info + disclaimer added to AOC intro modal

--- client/src/components/timeCalc.js
+++ client/src/components/timeCalc.js
@@ -58,70 +58,27 @@ function getCycleMoon (end) {
   )
 }
 
-
-//var newdate = new Date();
-//var nextdate = newdate.setDate(artsHouseDate.getDate() + 3);
-
 var suncalctimes = SunCalc.getTimes(date, lat, long);  
 var sunsetMilliseconds = Date.parse(suncalctimes.sunset);
 var countdownMilli = sunsetMilliseconds - date;
 
 var sunsethours = checknum(suncalctimes.sunset.getUTCHours() + 10);
 var sunsetmins = suncalctimes.sunset.getMinutes();
-var moontimes = SunCalc.getMoonIllumination(date, lat, long);
-var phase = moontimes.phase; 
-phase = Math.round(phase * 100);
-var phasedeg = 3.6 * phase;
-var dividerstyle = {transform: "rotate3d(0, 1, 0, " + phasedeg + "deg)",}
-
-/*var nextmoon = SunCalc.getMoonIllumination(nextdate, lat, long); 
-var nextphase = nextmoon.phase; 
-nextphase = Math.round(nextphase * 100);
-var nextphasedeg = 3.6 * nextphase; 
-var nextdividerstyle = {transform: "rotate3d(0, 1, 0, " + nextphasedeg + "deg)",}
-
-var liveDate = new Date('Sun Sep 25 2022');
-var liveSun = SunCalc.getTimes(liveDate, lat, long);
-var liveMoon = SunCalc.getMoonIllumination(liveDate, lat, long);
-var livePhase = liveMoon.phase;
-livePhase = Math.round(livePhase * 100);
-var livePhaseDeg = 3.6 * phase;
-var liveDividerStyle = {transform: "rotate3d(0, 1, 0, " + livePhaseDeg + "deg)",}*/
 
 //checking whether hours evaluations are above 24, if so removing 24 to get their AM time value in hours; also reevaluates date object if UTC date is yesterday
 function checknum (num) {
     if (num > 24) {
       num = num - 24;
-      //date.setDate(date.getUTCDate() + 1); //or run this in an if/else statement above
     } else {
       num = num; 
     }
       return num
   }
 
-export const sunSetText = <><p>The track changes at sunset. Today, the sun sets at {sunsethours}:{sunsetmins}pm, in {countdownMilli} milliseconds. *turn this value into a more legible countdown*</p>
+export const sunSetText = <><p>The track changes at sunset. Today, the sun sets at {sunsethours}:{sunsetmins}pm, in {countdownMilli} milliseconds. *turn this value into a more legible countdown* Maybe countdown could happen based on how many sunsets b/w now and next change</p>
 </>
 
-export const currentMoonShape = <>
-<div className="moonsphere">
-<div className=
-{phasedeg > 180 
-    ? "moondark hemisphere"
-    : "moonlight hemisphere"
-  }
-></div>
-  
-<div className=
-  {phasedeg > 180 
-  ? "moonlight hemisphere"
-  : "moondark hemisphere"
-  }
-></div>
-  
-<div className="moondivider" style={dividerstyle}></div>
-</div>
-</>
-
+export const currentMoonShape = getCycleMoon(date);
 export const firstMoonShape = getCycleMoon(cycleEndDates[0]);
 export const secondMoonShape = getCycleMoon(cycleEndDates[1]);
 export const thirdMoonShape = getCycleMoon(cycleEndDates[2]);
@@ -132,66 +89,9 @@ export const seventhMoonShape = getCycleMoon(cycleEndDates[6]);
 export const eighthMoonShape = getCycleMoon(cycleEndDates[7]);
 export const ninthMoonShape = getCycleMoon(cycleEndDates[8]);
 
-
-
-//above is adapted from this code https://dev.to/thormeier/use-your-i-moon-gination-lets-build-a-moon-phase-visualizer-with-css-and-js-aih
-/*
-export const nextMoonShape = <>
-<div className="moonsphere">
-<div className=
-{nextphasedeg > 180 
-    ? "moondark hemisphere"
-    : "moonlight hemisphere"
-  }
-></div>
-  
-<div className=
-  {nextphasedeg > 180 
-  ? "moonlight hemisphere"
-  : "moondark hemisphere"
-  }
-></div>
-  
-<div className="moondivider" style={nextdividerstyle}></div>
-</div>
-</>
-
-export const liveMoonShape = <>
-<div className="moonsphere">
-<div className=
-{nextphasedeg > 180 
-    ? "moondark hemisphere"
-    : "moonlight hemisphere"
-  }
-></div>
-  
-<div className=
-  {nextphasedeg > 180 
-  ? "moonlight hemisphere"
-  : "moondark hemisphere"
-  }
-></div>
-  
-<div className="moondivider" style={liveDividerStyle}></div>
-</div>
-</>
-*/
 /* SunCalc values for sunset changeover (maybe use these values for transitioning b/w tracks) — 
 goldenHour | evening golden hour starts
 sunsetStart | sunset starts (bottom edge of the sun touches the horizon)
 sunset | sunset (sun disappears below the horizon, evening civil twilight starts)
 dusk | dusk (evening nautical twilight starts)
 */
-
-//export const artsHouseDate = getArtsHouseDate();
-
-/*function getArtsHouseDate () {  
-  if (nowhours > 24) {
-    date.setDate(date.getUTCDate() + 1);
-    nowhours = nowhours - 24;
-  };
-  date.setHours(nowhours);
-  date.setMinutes(date.getUTCMinutes());
-  
-  return date;
-}*/
---
+++ client/src/components/whereModal.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react';
+
+const IP_API = 'https://ipinfo.io/json?token=9f90c3a3f131ff' 
+const NATIVE_LAND_API = 'https://native-land.ca/api/index.php'
+var youlat;
+var youlong; 
+
+export default function WhereAreYou () {
+  const [whereTerritories, getTerritories] = useState([]);
+  
+  useEffect(() => {
+    getPotentialLocations()
+    .then(data =>
+      getTerritories(data)
+    );
+   }, [])
+
+  return (
+  <>
+  {whereTerritories}
+  </>
+  )
+}
+
+async function getPotentialLocations() {  
+  const ipData = await getLocationDataFromIp()
+  if (!ipData) return <>
+  <span>We either couldn't access your IP, or couldn't find information about where you are. 
+    This doesn't mean you're not on indigenous land.*
+  </span>
+  </>
+  const territories = await getTerritoriesFromLngLat(youlat, youlong)
+  
+  if (territories.length === 1) {
+    return (
+    <><span>Your IP suggests that you may be on <a href={territories[0].properties.description} target='_blank'>{territories[0].properties.Name}</a> country, otherwise known as {ipData.commonName}.*</span></>
+    )
+  } else if (territories.length > 1) {
+    return <><span>Your IP suggests that you may be on <a href={territories[0].properties.description} target='_blank'> {territories[0].properties.Name}</a> and/or <a href={territories[1].properties.description} target='_blank'> {territories[1].properties.Name}</a> country, otherwise known as {ipData.commonName}.*</span></>
+  } else {
+    return <><span>Your IP suggests that you may be in {ipData.commonName}. We cannot find information about indigenous nations at this location, but this does not mean that there aren't any.*</span></>
+  }
+  
+  }
+  
+  function getLocationDataFromIp() {
+    
+  return fetch(IP_API)
+  .then((response) => response.json())
+  .then((data) => {
+  const latLng = extractLngLat(data)
+  console.log(latLng)
+  return latLng
+    ? {
+        ...latLng,
+        commonName: extractLocationString(data) ?? '',
+      }
+    : null
+  })
+  }
+  
+  function extractLngLat(data) {
+  if (!data) return null
+  if (!data.loc || typeof data.loc !== 'string') return null
+  const locArray = data.loc.split(',')
+  if (locArray.length !== 2) return null
+  youlat = parseFloat(locArray[0])
+  youlong = parseFloat(locArray[1])
+  
+  if (youlat == null || youlong == null) return null
+  return {
+  youlat,
+  youlong,
+  }
+  }
+  
+  function getTerritoriesFromLngLat(x, y) {
+  return fetch(
+  `${NATIVE_LAND_API}?maps=territories&position=${x},${y}`
+  )
+  .then((res) => res.json())
+  .catch((e) => {
+  console.error(e)
+  return []
+  })
+  }
+
+  function toTerritoryInfo(features) {
+    return features
+      .map((feature) => {
+        const name = feature.properties.Name
+        const link = feature.properties.description
+        return name
+          ? {
+              name,
+              link,
+            }
+          : undefined
+      })
+  }
+  
+  
+  function extractLocationString(data) {
+  if (!data) return null
+  if (!data.city && !data.region) return null
+  const strings = []
+  data.city && strings.push(data.city)
+  data.region && strings.push(data.region)
+  data.country && strings.push(data.country)
+  return strings.join(', ')
+  }
+
+
