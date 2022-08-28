b7a3bd7595de3b5aa59c0c4a609246f091ca8304
hase <patrickahase@gmail.com>
2022-08-28 20:35:13 +1000

    sever selection order

--- api/dbService.js
+++ api/dbService.js
@@ -23,26 +23,15 @@ connection.connect((err) => {
 });
 
 // to avoid sql injections use the cycleTable number to select from array of queries
-const sqlQueries = [ `SELECT * FROM CYCLE_1; INSERT INTO CYCLE_1 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?);`,
-                     `SELECT * FROM CYCLE_2; INSERT INTO CYCLE_2 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?);`,
-                     `SELECT * FROM CYCLE_3; INSERT INTO CYCLE_3 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?);`,
-                     `SELECT * FROM CYCLE_4; INSERT INTO CYCLE_4 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?);`,
-                     `SELECT * FROM CYCLE_5; INSERT INTO CYCLE_5 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?);`,
-                     `SELECT * FROM CYCLE_6; INSERT INTO CYCLE_6 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?);`,
-                     `SELECT * FROM CYCLE_7; INSERT INTO CYCLE_7 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?);`,
-                     `SELECT * FROM CYCLE_8; INSERT INTO CYCLE_8 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?);`,
-                     `SELECT * FROM CYCLE_9; INSERT INTO CYCLE_9 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?);`];
-
-/* const sqlQueries = {
-getScheduleQ:       `SELECT CURRENT_ARTIST AS 'currentArtist', CURRENT_PROMPT AS 'currentPrompt', PROMPT_TYPE AS 'promptType',
-                    EMOJI_1 AS 'emoji1', EMOJI_2 AS 'emoji2', EMOJI_3 AS 'emoji3' FROM LIVE_SCHEDULE WHERE id = 1;`, 
-getResponseUpdateQ: `SELECT * FROM RESPONSES WHERE (RESPONSE_DATETIME > now() - interval 3 minute) AND (id > ?);`, 
-getEmojisQ:         `SELECT ALT_TEXT AS 'altText', EMOJI_STRING AS 'emojiString' FROM EMOJI_STORAGE WHERE NAME = (? OR ? OR ?);`,
-insertResponseQ:    `INSERT INTO RESPONSES (RESPONSE, RESPONSE_TYPE) VALUES (?, ?);`
-} */
-
-
-
+const sqlQueries = [ `INSERT INTO CYCLE_1 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?); SELECT * FROM CYCLE_1;`,
+                     `INSERT INTO CYCLE_2 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?); SELECT * FROM CYCLE_2;`,
+                     `INSERT INTO CYCLE_3 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?); SELECT * FROM CYCLE_3;`,
+                     `INSERT INTO CYCLE_4 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?); SELECT * FROM CYCLE_4;`,
+                     `INSERT INTO CYCLE_5 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?); SELECT * FROM CYCLE_5;`,
+                     `INSERT INTO CYCLE_6 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?); SELECT * FROM CYCLE_6;`,
+                     `INSERT INTO CYCLE_7 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?); SELECT * FROM CYCLE_7;`,
+                     `INSERT INTO CYCLE_8 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?); SELECT * FROM CYCLE_8;`,
+                     `INSERT INTO CYCLE_9 (RESPONSE, RESPONSE_TYPE) VALUES (?, ?); SELECT * FROM CYCLE_9;`];
 
 class DbService {
 
