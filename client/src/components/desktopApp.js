7b674cdb5d0ef73f5ecac2bc5b022120c3593313
Hase <patrickahase@gmail.com>
2022-09-24 17:07:54 +1000

    small change

--- client/src/components/desktopApp.js
+++ client/src/components/desktopApp.js
@@ -13,7 +13,7 @@ import VideoStreamPlayer from './videoStreamPlayer';
 
 export default function DesktopApp(props){
 
-  const [currentModalPage, setCurrentModalPage] = useState(0);
+  const [currentModalPage, setCurrentModalPage] = useState(1);
   const [showOverlay, setShowOverlay] = useState(true);
 
   return (
