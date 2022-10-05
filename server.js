77551e4f4a1984d6bd80816419e45ab92b79353b
hase <patrickahase@gmail.com>
2022-10-05 11:31:47 +1100

    now

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ app.use(cors());
 const endPoints = require('./api/room2ClientAPI');
 
 // direct to build folder
-app.use(express.static(path.join(__dirname, 'client/build')));
-
+//app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/*', (req, res) => {
   res.sendFile(__dirname, '/client/build/index.html');
