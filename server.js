06f01fcd89139d2babdacf5be0ac4d33269ef4de
Patrick McMahon <patrick.mcmahon@rmit.edu.au>
2025-10-19 17:13:25 +1100

    remove storage

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+var cors = require("cors");
+
+//init app and body parser
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+// direct to build folder
+app.use("/", express.static(path.join(__dirname, "home")));
+
+// serve index - '/' or '*' ?
+app.get("*", (req, res) => {
+  console.log(1);
+  res.sendFile(__dirname, "/home/index.html");
+});
+
+// look more into process stuff
+const port = process.env.PORT || 33064;
+
+// listen on 33061
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
