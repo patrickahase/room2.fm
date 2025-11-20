ec9180a697b9badc22846469cbbc55b0a66718ef
Patrick McMahon <patrick.mcmahon@rmit.edu.au>
2025-11-21 09:27:53 +1100

    aSYNC RESPONSES

--- client/src/sync/src/components/drawingCanvas.jsx
+++ client/src/sync/src/components/drawingCanvas.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
-//import { fabric } from 'fabric';
-import { fabric } from './fabric';
+import { fabric } from 'fabric';
+
 
 export default function DrawingCanvas(props){
   
